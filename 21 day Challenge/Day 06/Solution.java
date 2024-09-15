0d4edc940295bc924d71503fe2837ff79113df42
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2024-09-15 13:03:14 +0530

    Add Solution of Day 6 of 21 Day of Coding Challenge With GFG

---
+++ 21 day Challenge/Day 06/Solution.java
@@ -0,0 +1,22 @@
+class Solution
+{
+    Node head;
+    //Function to convert binary tree to doubly linked list and return it.
+    Node bToDLL(Node root)
+    {
+	//  Your code here
+	    if(root == null)
+			return null;
+		
+		bToDLL(root.right);
+		root.right = head;
+		
+		if(head != null)
+			head.left = root;
+			
+		head = root;
+		bToDLL(root.left);
+		
+		return head;
+    }
+}
