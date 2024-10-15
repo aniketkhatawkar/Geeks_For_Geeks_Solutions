db7e9296d71c07103d755bb623012089d76415d4
Aniket A Khatavkar <aniketkhatawkar2001@gmail.com>
2024-10-15 22:47:04 +0530

    add Solution of POTD of 15 October 2024 GFG

---
+++ POTD/2024/October/15 October 2024/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    // Function to count the number of subarrays which adds to the given sum.
+    static int subArraySum(int arr[], int tar) {
+        // add your code here
+        HashMap<Integer,Integer>hm = new HashMap<>();
+        int sum = 0;
+        int ans = 0;
+        hm.put(0,1);
+        for(int i=0;i<arr.length;i++){
+            sum = sum + arr[i];
+            if(hm.containsKey(sum-tar)){
+                ans = ans + hm.get(sum-tar);
+            }
+            hm.put(sum,hm.getOrDefault(sum,0)+1);
+        }
+        return ans;
+    }
+}
