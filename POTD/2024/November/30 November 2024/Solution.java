a2ff9300e7c5eb9021dbf7a991ba36fb4cddd75d
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2024-11-30 21:39:03 +0530

    add Solution of POTD of 30 November 2024 GFG

---
+++ POTD/2024/November/30 November 2024/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    // Function is to check whether two strings are anagram of each other or not.
+    public static boolean areAnagrams(String s1, String s2) {
+
+        // Your code here
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+}
