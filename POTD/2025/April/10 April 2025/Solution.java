c9ed5c9e3852b2abfa5be52a03d74970a64a4696
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2025-04-10 16:34:32 +0530

    add Solution of POTD of 10 April 2025 GFG

---
+++ POTD/2025/April/10 April 2025/Solution.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    public int minCost(int[][] houses) {
+        // code here
+        int n = houses.length;
+        boolean[] inMST = new boolean[n];
+        int[] minDist = new int[n];
+        Arrays.fill(minDist, Integer.MAX_VALUE);
+
+        int totalCost = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        pq.offer(new int[]{0, 0});
+
+        while (!pq.isEmpty()) {
+            int[] top = pq.poll();
+            int cost = top[0];
+            int u = top[1];
+
+            if (inMST[u]) continue;
+
+            inMST[u] = true;
+            totalCost += cost;
+
+            for (int v = 0; v < n; v++) {
+                if (!inMST[v]) {
+                    int dist = manhattanDist(houses[u], houses[v]);
+                    if (dist < minDist[v]) {
+                        minDist[v] = dist;
+                        pq.offer(new int[]{dist, v});
+                    }
+                }
+            }
+        }
+
+        return totalCost;
+    }
+    
+    private int manhattanDist(int[] a, int[] b) {
+        return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+    }
+}
